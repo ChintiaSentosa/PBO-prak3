a3096479bb6f5ee09303828e633ba7496c0f58a7
ChintiaSentosa <66993826+ChintiaSentosa@users.noreply.github.com>
2021-10-07 11:38:27 +0800

    Add files via upload

---
+++ latihan7.java
@@ -0,0 +1,11 @@
+public class latihan7{
+	public static void main(String args[]){
+		int i;
+		for (i=1; i<9; i++)
+		{
+			System.out.println(i);
+			//jika menggunakan system.out.println maka output berada di baris baru berurutan ke bawah 
+			//, sedangkan system.out.print maka output berada pada baris yang sama 
+		}
+	}
+}
